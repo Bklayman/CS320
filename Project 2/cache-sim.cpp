58ef7ce0b963c50d52cd184140d70721994ff8d7
Brendan Klayman <bklayma1@binghamton.edu>
2021-11-12 16:00:43 -0500

    Project 2: Part 3 has some error in hot/cold and part 4 is done

--- Project 2/cache-sim.cpp
+++ Project 2/cache-sim.cpp
@@ -91,6 +91,118 @@ int setAssociative(vector<pair<char, unsigned int>> data, int associativity){
   return result;
 }
 
+vector<int> updateHeat(vector<int> heatBits, int index){
+  int max = heatBits.size() - 1, min = 0;
+  int prevMax = -1, prevMin = -1;
+  int runs = (int)log2(heatBits.size() + 1);
+  for(int i = 0; i < runs; i++){
+    prevMax = max;
+    prevMin = min;
+    int checkIndex = (max + min) / 2;
+    if(index <= checkIndex){
+      heatBits[checkIndex] = 0;
+      max = checkIndex;
+    } else {
+      heatBits[checkIndex] = 1;
+      min = checkIndex;
+    }
+  }
+  return heatBits;
+}
+
+int hotCold(vector<pair<char, unsigned int>> data){
+  int result = 0, associativity = 512;
+  unsigned int cache[associativity];
+  int validBits[associativity];
+  int mask = (1 << 5) - 1;
+  vector<int> heatBits;
+
+  for(int i = 0; i < associativity; i++){
+    validBits[i] = 0;
+    if(i != associativity - 1){
+      heatBits.push_back(0);
+    }
+  }
+
+  for(int i = 0; i < data.size(); i++){
+    unsigned int remainder = data[i].second & (~mask);
+    bool stored = false;
+    for(int j = 0; j < associativity; j++){
+      if(validBits[j] != 0 && cache[j] == remainder){
+        result++;
+        stored = true;
+        heatBits = updateHeat(heatBits, j);
+        break;
+      }
+    }
+    if(!stored){
+      int rangeMin = 0;
+      int rangeMax = associativity - 1;
+      while(rangeMin != rangeMax){
+        int checkHeat = (rangeMin + rangeMax) / 2;
+        if(heatBits[checkHeat] == 0){
+          rangeMin = checkHeat + 1;
+          heatBits[checkHeat] = 1;
+        } else {
+          rangeMax = checkHeat;
+          heatBits[checkHeat] = 0;
+        }
+      }
+      cache[rangeMin] = remainder;
+      validBits[rangeMin] = 1;
+    }
+  }
+
+  return result;
+}
+
+int noWrite(vector<pair<char, unsigned int>> data, int associativity){
+  int powNum = pow(2, 14), result = 0, numSets = (powNum / 32) / associativity;
+  unsigned int cache[numSets][associativity];
+  int validBits[numSets][associativity];
+  vector<vector<int>> leastUsed;
+  int mask = (1 << (5 + (int)log2(numSets))) - 1;
+
+  for(int i = 0; i < numSets; i++){
+    for(int j = 0; j < associativity; j++){
+      validBits[i][j] = 0;
+    }
+    vector<int> curSet;
+    leastUsed.push_back(curSet);
+  }
+
+  for(int i = 0; i < data.size(); i++){
+    int block = (data[i].second / 32) % (powNum / 32);
+    int setNum = block % (numSets);
+    unsigned int remainder = data[i].second & (~mask);
+    bool stored = false;
+    bool loaded = (data[i].first == 'L');
+    for(int j = 0; j < associativity; j++){
+      if(loaded && validBits[setNum][j] == 0){
+        validBits[setNum][j] = 1;
+        cache[setNum][j] = remainder;
+        stored = true;
+        leastUsed[setNum].push_back(j);
+        break;
+      }
+      if(validBits[setNum][j] != 0 && cache[setNum][j] == remainder){
+        result++;
+        stored = true;
+        leastUsed[setNum].erase(remove(leastUsed[setNum].begin(), leastUsed[setNum].end(), j), leastUsed[setNum].end());
+        leastUsed[setNum].push_back(j);
+        break;
+      }
+    }
+    if(!stored && loaded){
+      cache[setNum][leastUsed[setNum][0]] = remainder;
+      leastUsed[setNum].push_back(leastUsed[setNum][0]);
+      leastUsed[setNum].erase(leastUsed[setNum].begin());
+    }
+  }
+
+  return result;
+}
+
 int main(int argc, char** argv){
   if(argc != 3){
     cout << "Usage: ./cache-sim <input file> <output file>" << endl;
@@ -111,7 +223,13 @@ int main(int argc, char** argv){
     outputFile << setAssociative(inputData, 8) << "," << numOps << "; ";
     outputFile << setAssociative(inputData, 16) << "," << numOps << "; " << endl;
     outputFile << setAssociative(inputData, 512) << "," << numOps << "; " << endl;
+    outputFile << hotCold(inputData) << "," << numOps << "; " << endl;
+    outputFile << noWrite(inputData, 2) << "," << numOps << "; ";
+    outputFile << noWrite(inputData, 4) << "," << numOps << "; ";
+    outputFile << noWrite(inputData, 8) << "," << numOps << "; ";
+    outputFile << noWrite(inputData, 16) << "," << numOps << "; " << endl;
   } else {
     cout << "Unable to open " << argv[2] << endl;
   }
+
 }
